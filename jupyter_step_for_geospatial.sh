7e4ded09022e8bccbc9d8c784891d4b56cb9b97f
Yuan Zhao <yuanzhaoYZ@users.noreply.github.com>
2018-03-16 13:28:34 -0500

    Update jupyter_step_for_geospatial.sh

--- jupyter_step_for_geospatial.sh
+++ jupyter_step_for_geospatial.sh
@@ -6,8 +6,17 @@ echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/anaconda/bin/jupyter" | sudo tee
 echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser --ip=0.0.0.0'" | sudo tee --append /etc/spark/conf/spark-env.sh > /dev/null
 
 # optional
-conda create -n playground_py36 --copy -y -q python=3.6 pyspark numpy pandas pysal scipy ipykernel
+sudo yum -y install git
+git config --global user.email "zhao.yuan.work@gmail.com"
+git config --global user.name "Yuan Zhao"
+echo "y" |conda create -n playground_py36 --copy -y -q python=3.6 pyspark numpy pandas pysal scipy ipykernel
 source activate playground_py36
 echo "y" |pip install awscli
 echo "y" |pip install https://github.com/python-visualization/folium/archive/master.zip
 conda install -y -c ioos geopandas=0.2.1
+source deactivate
+
+screen -dmS jupyter
+screen -r jupyter
+conda install -y nb_conda
+pyspark --master yarn
