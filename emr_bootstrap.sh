30f3bf99194d521f722e44c8d27cda1d9b52f565
Yuan Zhao <yuanzhaoYZ@users.noreply.github.com>
2018-03-16 11:49:45 -0500

    use anaconda3

--- emr_bootstrap.sh
+++ emr_bootstrap.sh
@@ -3,12 +3,12 @@
 sudo yum install -y htop tmux
 
 # download and install anaconda:
-wget -q https://repo.continuum.io/archive/Anaconda2-4.4.0-Linux-x86_64.sh -O ~/anaconda2.sh
-/bin/bash ~/anaconda2.sh -b -p $HOME/anaconda
+wget -q https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh -O ~/anaconda3.sh
+/bin/bash ~/anaconda3.sh -b -p $HOME/anaconda
 echo -e '\nexport SPARK_HOME=/usr/lib/spark\nexport PATH=$HOME/anaconda/bin:$PATH' >> ~/.bashrc && source ~/.bashrc
 
 # cleanup:
-rm ~/anaconda2.sh
+rm ~/anaconda3.sh
 
 # enable https://github.com/mozilla/jupyter-spark:
 sudo mkdir -p /usr/local/share/jupyter
